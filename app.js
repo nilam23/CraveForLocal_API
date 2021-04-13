842fad39bd658b9310c7d02f14e27b7731b5482d
adhikarisonit <sonitkumaradhikari@gmail.com>
2021-04-13 15:02:21 +0530

    import export changes

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 const User = require("./models/userCollection");
+const Item = require("./models/itemCollection");
+const Order = require("./models/orderCollection");
 
-mongoose.connect("mongodb://localhost/CraveForLocal", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-    .then(() => console.log("MONGODB CONNECTION SUCCESSFUL."))
-    .catch((err) => console.log(err));
+mongoose
+  .connect("mongodb://localhost/CraveForLocal", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MONGODB CONNECTION SUCCESSFUL."))
+  .catch((err) => console.log(err));
 
-console.log(User);
+console.log(Item);
+console.log(Order);
