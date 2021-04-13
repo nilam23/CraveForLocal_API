c2d2d76b8eb38bff13638f90a7561a6e8c5f1911
adhikarisonit <sonitkumaradhikari@gmail.com>
2021-04-13 14:53:53 +0530

    adding db models

---
+++ models/itemCollection.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const itemSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Item = mongoose.model("Item", itemSchema);
+
+export default Item;
---
+++ models/orderCollection.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    itemId: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Item",
+      },
+    ],
+    qty: [Number],
+    price: [Number],
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    orderedAt: {
+      type: Date,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
