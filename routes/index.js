42edc4eab2c53e55a3e49d0a910f28928e1eb28e
Nilam Pathak <nilampathak72@gmail.com>
2021-05-27 23:29:17 +0530

    password reset added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 const User = require("../models/userCollection");
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const isLoggedin = (req, res, next) => {
     if (req.session.user_id) {
@@ -25,7 +28,7 @@ router.post("/signup", async (req, res) => {
         const { email, password } = req.body;
         const user = await User.findOne({ email });
         if (!user) {
-            var hashedPassword = await bcrypt.hash(password, 12);
+            var hashedPassword = await bcrypt.hash(password, process.env.BCRYPT_SALT);
             const newUser = new User({
                 email,
                 password: hashedPassword,
@@ -119,6 +122,79 @@ router.post("/admin/vendors/new", async (req, res) => {
 router.post("/logout", (req, res) => {
     req.session.user_id = null;
     res.redirect("/items");
+});
+
+router.get("/forgotpassword", (req, res) => {
+    res.render("forgotPasswordForm");
+});
+
+router.post("/forgotpassword", async (req, res) => {
+    try {
+        const email = req.body.email;
+        const user = await User.findOne({ email });
+        if (!user) {
+            req.session.message = {
+                type: "danger",
+                content: "Email entered is invalid. Please sign up."
+            };
+            res.redirect("/signup");
+        } else {
+            //Creating a one time link valid for a specific amount of time
+            const secret = JWT_SECRET + user.password;
+            const payload = {
+                email: user.email,
+                id: user._id
+            };
+            const token = jwt.sign(payload, secret, { expiresIn: '15m' });
+            const link = `http://localhost:3000/resetpassword/${user._id}/${token}`;
+            //send this link to user via email
+            console.log(link);
+            res.send("Password Reset link sent to your email.");
+        }
+
+    } catch (err) {
+        console.log(`---FORGOT PASSWORD ERROR: ${err}.---`);
+        res.redirect("/signin");
+    }
+});
+
+router.get("/resetpassword/:id/:token", async (req, res) => {
+    const { id, token } = req.params;
+    const user = await User.findOne({ _id: id });
+    if (!user) {
+        res.send("Invalid user.");
+    } else {
+        const secret = JWT_SECRET + user.password;
+        try {
+            const payload = jwt.verify(token, secret);
+            res.render("resetPasswordForm", { email: user.email });
+        } catch (error) {
+            console.log(`---FORGOT PASSWORD ERROR: ${error}.---`);
+            res.render("")
+        }
+    }
+});
+
+router.post("/resetpassword/:id/:token", async (req, res) => {
+    const { id, token } = req.params;
+    const { password, confirmedPassword } = req.body;
+    const user = await User.findOne({ _id: id });
+    if (!user) {
+        res.send("Invalid user.");
+    } else {
+        const secret = JWT_SECRET + user.password;
+        try {
+            const payload = jwt.verify(token, secret);
+            if (password == confirmedPassword) {
+                const newHashedPassword = await bcrypt.hash(password, 12);
+                user.password = newHashedPassword;
+                await user.save();
+                res.redirect("/signin");
+            }
+        } catch (err) {
+            console.log(`---PASSWORD RESET ERROR: ${err}.---`);
+        }
+    }
 });
 
 module.exports = router;
